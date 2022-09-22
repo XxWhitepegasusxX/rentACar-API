24c8e7f4ecb19c1032b8828e957a2a75361f0c3c
Alexander Almeida <alexanderalmeida3040@gmail.com>
2022-09-22 18:48:25 +0000

    prisma added

---
+++ src/modules/cars/useCases/createCategory/createCategoryController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { CreateCategoryUseCase } from "./createCategoryUseCase";
+
+class CreateCategoryController{
+    constructor(private createCategoryUseCase: CreateCategoryUseCase){}
+
+    async handle(req: Request, res: Response): Promise<Response>{
+        const {name, description} = req.body;
+
+        await this.createCategoryUseCase.execute({name, description})
+
+        return res.status(201).send()
+    }
+}
+export { CreateCategoryController }
